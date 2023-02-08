45e7ceb724b855b08acd5e39920396afeab4300c
mstango <github@stango.org>
2023-02-08 11:07:14 -0500

    Added JSON output option.

--- web_thermo.py
+++ web_thermo.py
@@ -1,17 +1,18 @@
 from bottledaemon import daemon_run
-from bottle import route, run, template, static_file, TEMPLATE_PATH
+from bottle import route, run, template, static_file, TEMPLATE_PATH, response
 from datetime import datetime
 from pathlib import Path
 from decouple import config
 import Adafruit_DHT
 import time
+import json
 
 TEMPLATE_PATH.insert(0, str(Path(__file__).parent))
-datafilelocation = "{}/{}".format(str(Path(__file__).parent),"data.txt")
+DATA_FILE_LOCATION = "{}/{}".format(str(Path(__file__).parent),"data.txt")
 
 # Read data from the file
 def read_data_file():
-    with open(datafilelocation, mode="r") as datafile:
+    with open(DATA_FILE_LOCATION, mode="r") as datafile:
         data = datafile.readlines()
         temperature = data[0].rstrip()
         humidity = data[1].rstrip()
@@ -35,6 +36,15 @@ def index():
             }
 
     return template('template.tpl', data)
+
+@route('/raw')
+def rawdata():
+    temperature, humidity, date, time = read_data_file()
+    response.headers['Content-Type'] = 'application/json'
+    return json.dumps({'room': config('ROOM_NAME'),
+                       'timestamp': f'{date}T{time}',
+                       'temperature': temperature,
+                       'humidity': humidity})
 
 if __name__ == '__main__':
     daemon_run(host='0.0.0.0', port=config('WEBSERVER_PORT'))
